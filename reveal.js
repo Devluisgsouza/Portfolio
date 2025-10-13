35bed4737279e42752ed19e1cd3fdc992156685a
Devluisgsouza <devlgsouza@gmail.com>
2025-10-13 16:05:44 -0300

    ajuste efect

--- reveal.js
+++ reveal.js
@@ -1,56 +1,76 @@
 
 window.see = ScrollReveal({reset:true})
 
-// TOPO DO SITE
+// SITE TOP
 
 see.reveal('.efect-txt-top',{
-    duration: 2000,
+    duration: 1500,
     distance: '90px',
     origin: 'left',
 })
 
 see.reveal('.efetc-img-top',{
-    duration: 2000,
+    duration: 1500,
     distance: '90px',
     origin: 'right',
     delay: 500
 })
 
 see.reveal('.title',{
-    duration: 2000,
+    duration: 1500,
     distance: '90px',
     origin: 'right',
 })
 
-see.reveal('.flex',{
-    duration: 2000,
-    distance: '90px',
-    origin: 'left',
-})
+// SITE PROFILE
 
-see.reveal('.img-port-1',{
-    duration: 2000,
+see.reveal('.efect-profile',{
+    duration: 1500,
     distance: '90px',
     origin: 'left',
-})
-
-see.reveal('.img-port-2',{
-    duration: 2000,
-    distance: '90px',
-    origin: 'left',
-    delay: 500
-})
-
-see.reveal('.img-port-3',{
-    duration: 2000,
-    distance: '90px',
-    origin: 'left',
-    delay: 1000
 })
 
 see.reveal('.icons',{
-    duration: 2000,
+    duration: 1500,
     distance: '90px',
     origin: 'top',
     delay: 100
 })
+
+// SITE PROJECTS
+
+see.reveal('.img-port-1',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'left',
+    delay: 300
+})
+
+see.reveal('.img-port-2',{
+    duration: 1500,
+    distance: '90px',
+    delay: 300
+})
+
+see.reveal('.img-port-3',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'right',
+    delay: 300
+})
+
+// SITE CONTACTS
+
+see.reveal('.efect-contacts',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'left',
+})
+
+see.reveal('.social-icons',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'top',
+    delay: 100
+})
+
