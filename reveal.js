7a1290c93183062db635dcd3e469dc9f30acd2bd
Devluisgsouza <devlgsouza@gmail.com>
2025-10-13 12:04:05 -0300

    aplicação scroll

---
+++ reveal.js
@@ -0,0 +1,34 @@
+
+window.see = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+
+see.reveal('.efect-txt-top',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+})
+
+see.reveal('.efetc-img-top',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'right',
+    delay: 500
+})
+
+see.reveal('.title',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'right',
+})
+
+see.reveal('.flex',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+})
+
+see.reveal('.icons',{
+    duration: 2000,
+    distance: '90px',
+})
