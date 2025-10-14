fdfe1e763c8b3cbf09cad54220a142ebaa1e0982
Devluisgsouza <devlgsouza@gmail.com>
2025-10-14 02:19:38 -0300

    finalização menu mob

--- js/btn-menu-mob.js
+++ js/btn-menu-mob.js
@@ -4,6 +4,7 @@ let line1 = document.querySelector('.line-menu-mob1')
 let line2 = document.querySelector('.line-menu-mob2')
 let menuMobile = document.querySelector('#menu-mobile')
 let body = document.querySelector('body')
+let linksMenu = document.querySelectorAll('#menu-mobile a');
 
 btnMenuMob.addEventListener("click", ()=>{
     line1.classList.toggle('ativo1')
@@ -11,3 +12,13 @@ btnMenuMob.addEventListener("click", ()=>{
     menuMobile.classList.toggle('open')
     body.classList.toggle('no-overflow')
 })
+
+linksMenu.forEach(link => {
+    link.addEventListener("click", () => {
+        // Remove as classes de animação
+        line1.classList.remove('ativo1');
+        line2.classList.remove('ativo2');
+        menuMobile.classList.remove('open');
+        body.classList.remove('no-overflow');
+    });
+});
